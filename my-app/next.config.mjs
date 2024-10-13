ad665dd8d61d58e4c1ba3d9b628468ea9b5c6e97
m3lander <melanderhgs@gmail.com>
2024-10-13 18:08:17 +0100

    Update next.config.mjs to enable static export

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // This tells Next.js to export the static files
+  };
+  
+  export default nextConfig;
+  
