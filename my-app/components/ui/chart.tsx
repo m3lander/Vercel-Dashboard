1a78d7edda55e9f50cceea9de828868ad2e22230
m3lander <melanderhgs@gmail.com>
2024-10-13 17:44:04 +0100

    trying again

--- my-app/components/ui/chart.tsx
+++ my-app/components/ui/chart.tsx
@@ -68,10 +68,9 @@ const ChartContainer = React.forwardRef<
 ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const colorConfig = Object.entries(config).filter(
-    ([__, config]) => config.theme || config.color
-  )
+    ([, config]) => config.theme || config.color
+  );
 
   if (!colorConfig.length) {
     return null
